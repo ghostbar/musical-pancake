c96c94f18b8d145c3aa6e8aded69dc4c5dd124d6
Jose-Luis Rivas <me@ghostbar.co>
2016-01-02 16:25:00 -0500

    v7

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,50 @@
 use std::fmt;
 
-struct List(Vec<i32>);
+struct City {
+    name: &'static str,
+    lat: f32,
+    lon: f32
+}
 
-impl fmt::Display for List {
+impl fmt::Display for City {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let List(ref vec) = *self;
+        let lat_c = if self.lat >= 0.0 { 'N' } else { 'S' };
+        let lon_c = if self.lon >= 0.0 { 'E' } else { 'W' };
 
-        try!(write!(f, "["));
+        write!(f, "{}: {:.3}º{} {:.3}º{}",
+               self.name, self.lat.abs(), lat_c, self.lon.abs(), lon_c)
+    }
+}
 
-        for (count, v) in vec.iter().enumerate() {
-            if count != 0 { try!(write!(f, ", ")); }
-            try!(write!(f, "{}", v));
-        }
+#[derive(Debug)]
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8
+}
 
-        write!(f, "]")
+impl fmt::Display for Color {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "RGB ({}, {}, {}) 0x{:02X}{:02X}{:02X}",
+               self.red, self.green, self.blue,
+               self.red, self.green, self.blue)
     }
 }
 
 fn main() {
-    let v = List(vec![3, 2, 1]);
-    println!("{}", v);
+    for city in [
+        City { name: "Dublin", lat: 53.347778, lon: -6.259722 },
+        City { name: "Oslo", lat: 59.95, lon: 10.75 },
+        City { name: "Vancouver", lat: 49.25, lon: -123.1 }
+    ].iter() {
+        println!("{}", *city);
+    }
+
+    for color in [
+        Color { red: 128, green: 255, blue: 90 },
+        Color { red: 0, green: 3, blue: 254 },
+        Color { red: 0, green: 0, blue: 0 }
+    ].iter() {
+        println!("{}", *color);
+    }
 }
