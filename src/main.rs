5f22e7203a6b450e48a7106fb7febba3f702ca2b
Jose-Luis Rivas <me@ghostbar.co>
2016-01-02 03:54:23 -0500

    v4

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
+#[derive(Debug)]
+struct Structure(i32);
+
+#[derive(Debug)]
+struct Deep(Structure);
+
 fn main() {
-    println!("{} days", 31);
+    println!("{:?} months in a year.", 12);
+    println!("{1:?}, {0:?} is the actor {name:?}.",
+             "Slater",
+             "Christian",
+             name="actor's");
 
-    println!("{0}, this is {1}. {1}, this is {0}.", "Alice", "Bob");
+    println!("Now {:?} will print!", Structure(3));
 
-    println!("{subject} {verb} {predicate}",
-             predicate="over the lazy dog",
-             verb="the quick brown fox",
-             subject="jumps");
-    
-    println!("{} of {:b} people know binary, the other half don't", 1, 2);
-
-    println!("{number:>width$}", number=1, width=6);
-
-    println!("My name is {0}, {1} {0}.", "Bond", "James");
-
-    println!("{number:.width$}", number=12.123, width=6);
+    println!("Only a 7? {:?}", Deep(Structure(7)));
 }
