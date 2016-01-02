14b357c82522e0ff480cc9ff4cb059ecb6f603bb
Jose-Luis Rivas <me@ghostbar.co>
2016-01-02 05:14:04 -0500

    v6

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,23 @@
 use std::fmt;
 
-#[derive(Debug)]
-struct MinMax(i64, i64);
+struct List(Vec<i32>);
 
-impl fmt::Display for MinMax {
+impl fmt::Display for List {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "({}, {})", self.0, self.1)
-    }
-}
+        let List(ref vec) = *self;
 
-#[derive(Debug)]
-struct Point2 {
-    x: f64,
-    y: f64
-}
+        try!(write!(f, "["));
 
-impl fmt::Display for Point2 {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "x: {}, y: {}", self.x, self.y)
+        for (count, v) in vec.iter().enumerate() {
+            if count != 0 { try!(write!(f, ", ")); }
+            try!(write!(f, "{}", v));
+        }
+
+        write!(f, "]")
     }
 }
 
 fn main() {
-    let minmax = MinMax(0, 14);
-
-    println!("Compare structures");
-    println!("Display: {}", minmax);
-    println!("Debug: {:?}", minmax);
-
-    let big_range = MinMax(-300, 300);
-    let small_range = MinMax(-3, 3);
-
-    println!("The big range is {big} and the small range is {small}",
-             small = small_range,
-             big = big_range);
-
-    let point = Point2 { x: 3.2, y: 7.4 };
-
-    println!("Compare points:");
-    println!("Display: {}", point);
-    println!("Debug: {:?}", point);
+    let v = List(vec![3, 2, 1]);
+    println!("{}", v);
 }
